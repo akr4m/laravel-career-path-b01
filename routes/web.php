d77c0b0520d7d5d6083cf6503e409d0b20121958
akr4m <akram@futurestation.net>
2025-08-10 22:42:46 +0600

    Add file upload functionality with validation and user feedback

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/upload', [UploadController::class, 'upload']);
+Route::post('/upload', [UploadController::class, 'store'])->name('upload.store');
