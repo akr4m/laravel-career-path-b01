f80e90cbdde1d5e66819884ca74546215a54c63e
akr4m <akram@futurestation.net>
2025-08-10 23:12:31 +0600

    Implement image gallery with upload, display, and delete functionality

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/upload', [UploadController::class, 'upload']);
 Route::post('/upload', [UploadController::class, 'store'])->name('upload.store');
+
+Route::get('/gallery', [ImageController::class, 'index']);
+Route::post('/gallery', [ImageController::class, 'store'])->name('gallery.store');
+Route::get('/gallery/{image}', [ImageController::class, 'destroy'])->name('gallery.destroy');
