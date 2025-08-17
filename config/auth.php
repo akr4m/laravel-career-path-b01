25e1109a029ac69a88d89adc277f77c54db61635
akr4m <akram@futurestation.net>
2025-08-17 23:07:03 +0600

    Implement authentication system with login, registration, and dashboard views; add Sanctum for API token management

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        // 'customers_web' => [
+        //     'driver' => 'session',
+        //     'provider' => 'customers',
+        // ],
     ],
 
     /*
@@ -64,6 +68,11 @@ return [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
+
+        // 'customers' => [
+        //     'driver' => 'eloquent',
+        //     'model' => App\Models\Customer::class,
+        // ],
 
         // 'users' => [
         //     'driver' => 'database',
